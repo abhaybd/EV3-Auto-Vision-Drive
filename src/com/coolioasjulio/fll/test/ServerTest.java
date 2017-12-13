1cdc71785e176b45c28473bf8444bc4dc31fbd1b
Abhay D <a.deshpande012@gmail.com>
2017-12-12 23:53:26 -0800

    Added test scripts to debug yuv conversion

---
+++ src/com/coolioasjulio/fll/test/ServerTest.java
@@ -0,0 +1,25 @@
+package com.coolioasjulio.fll.test;
+
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerTest {
+	public static void main(String[] args) throws IOException{
+		ServerSocket serverSocket = new ServerSocket(4444);
+		System.out.println("Waiting...");
+		Socket socket = serverSocket.accept();
+		System.out.println("Connected!");
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		int length = in.readInt();
+		System.out.println("Recieving " + length + " bytes");
+		byte[] bytes = new byte[length];
+		in.readFully(bytes);
+		serverSocket.close();
+		FileOutputStream file = new FileOutputStream("img.yuv");
+		file.write(bytes);
+		file.close();
+	}
+}
---
+++ src/com/coolioasjulio/fll/visiondrive/YUVTest.java
@@ -0,0 +1,22 @@
+package com.coolioasjulio.fll.visiondrive;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import lejos.hardware.lcd.LCD;
+
+public class YUVTest {
+	public static void main(String[] args) throws IOException{
+		ImageCapture ic = new ImageCapture();
+		byte[] bytes = ic.captureImage();
+		
+		LCD.drawInt(bytes.length, 0, 0);
+		
+		Socket socket = new Socket("192.168.1.87", 4444);
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		out.writeInt(bytes.length);
+		out.write(bytes);
+		socket.close();
+	}
+}
