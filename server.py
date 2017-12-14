f3d452e95a39796dad9d6eda5230f93f2d074737
Abhay D <a.deshpande012@gmail.com>
2017-12-13 18:16:04 -0800

    Fixed bug in udp thread of server.py
    The udp socket wasn't binded before use, so now it is.

--- server.py
+++ server.py
@@ -12,6 +12,8 @@ serversocket.listen()
 
 def udp_thread():
     udp_sock = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    udp_sock.bind((s.gethostname(),4445))
+    print('Waiting for udp ping!')
     while True:
         msg, addr = udp_sock.recvfrom(256)
         udp_sock.sendto(msg, addr)
